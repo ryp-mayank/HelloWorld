8291257e680df8ed776214e993f9265c5db054e4
mayank.agrawal <mayank.agrawal@airpush.com>
2016-02-18 16:59:11 +0530

    for allure

---
+++ src/test/java/my/company/tests/SearchTest.java
@@ -0,0 +1,29 @@
+package my.company.tests;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.lang.System;
+
+/**
+ * @author Dmitry Baev charlie@yandex-team.ru
+ *         Date: 28.10.13
+ */
+public class SearchTest {
+
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void searchTest() throws Exception {
+        System.out.println("this is mayank");
+    }
+
+}
+
